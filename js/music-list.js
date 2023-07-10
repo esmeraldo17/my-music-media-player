779b5bc8a3dda6049fcee9c2e7395542e2506051
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-07-10 15:46:56 +0100

    style: controls and  music-list && feat: allMusic array

---
+++ js/music-list.js
@@ -0,0 +1,26 @@
+ let allMusic = [
+    {
+        name: "Coracao Boemio",
+        artist: "Sidjay",
+        img:"song-1",
+        src: "song-1"
+    },
+    {
+        name: "When I'm Gone",
+        artist: "Eminem",
+        img:"song-2",
+        src: "song-2"
+    },
+    {
+        name: "Let Her Go",
+        artist: "Passenger",
+        img:"song-3",
+        src: "song-3"
+    },
+    {
+        name: "Aneis de Saturno",
+        artist: "Sidjay",
+        img:"song-4",
+        src: "song-4"
+    }
+ ]
